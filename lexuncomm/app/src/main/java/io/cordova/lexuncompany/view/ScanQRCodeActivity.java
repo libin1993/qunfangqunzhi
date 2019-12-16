9f8b8a3fb213e20d4dfe1d88d1e75b067aee3ade
1993911441@qq.com <LiBin1993911>
2019-12-16 17:46:39 +0800

    扫码

--- lexuncomm/app/src/main/java/io/cordova/lexuncompany/view/ScanQRCodeActivity.java
+++ lexuncomm/app/src/main/java/io/cordova/lexuncompany/view/ScanQRCodeActivity.java
@@ -59,7 +59,7 @@ public class ScanQRCodeActivity extends BaseActivity implements QRCodeView.Deleg
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Symbol.is_only_scan_center = true;
-        Symbol.scanType =  QrConfig.TYPE_QRCODE;
+        Symbol.scanType =  QrConfig.TYPE_CUSTOM;
         Symbol.doubleEngine = true;
         setContentView(R.layout.activity_scan_qrcode);
         ButterKnife.bind(this);
